dc45bfa0ea7e85927c8b7b14b670ad6653975fe8
Салабай Наталья <salabay.natalia89@gmail.com>
2024-04-17 15:01:30 +0300

    Выполняет задание Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import './functions.js';
 import { photoArray } from './photos.js';
+import { getMiniatures } from './getMiniatures.js';
 
-console.log(photoArray());
+// console.log(photoArray());
+console.log(getMiniatures());
+getMiniatures();
